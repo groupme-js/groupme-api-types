a6706d4b8d39fa27bfa89e58d1f5076b222e9e92
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-10-15 18:40:34 -0400

    Create APIAttachment.ts

---
+++ types/APIAttachment.ts
@@ -0,0 +1,73 @@
+export type APIAttachment =
+    Reply |
+    Image |
+    Mention |
+    Emoji |
+    Video |
+    Poll |
+    LinkedImage |
+    File |
+    CalendarEvent |
+    AutokickedMember;
+
+interface AutokickedMember {
+    type:    "autokicked_member",
+    user_id: string,
+}
+
+interface Emoji {
+    type:        "emoji",
+    placeholder: "�",
+    charmap:     Array<number[]>,
+}
+
+interface CalendarEvent {
+    type:     "event",
+    view:     View,
+    event_id: string,
+}
+
+enum View {
+    Brief = "brief",
+    Full = "full",
+}
+
+interface File {
+    type:    "file",
+    file_id: string,
+}
+
+interface Image {
+    type:        "image",
+    url:         string,
+    source_url?: string,
+}
+
+interface LinkedImage {
+    type: "linked_image",
+    url:  string,
+}
+
+interface Mention {
+    type:     "mentions",
+    loci:     Array<number[]>,
+    user_ids: string[],
+}
+
+interface Poll {
+    type:    "poll",
+    poll_id: string,
+}
+
+interface Reply {
+    type:          "reply",
+    user_id:       string,
+    reply_id:      string,
+    base_reply_id: string,
+}
+
+interface Video {
+    type:        "video",
+    preview_url: string,
+    url:         string,
+}
