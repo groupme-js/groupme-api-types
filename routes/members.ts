3555347a7a93e98037ddaf64905cc86c39ac6f83
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-10-16 16:00:39 -0400

    [add] routes\members.ts
    typings for member-related requests

---
+++ routes/members.ts
@@ -0,0 +1,57 @@
+import type { APIMember } from "../types/APIMember";
+
+/**
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#add-members
+ */
+export interface PostMembersBody {
+    members: ({
+        nickname: string;
+        guid: string;
+    } & ( // one of the following must exist in each entry
+            | { user_id: string; }
+            | { phone_number: string; }
+            | { email: string; }
+        )
+    )[];
+}
+
+/**
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#add-members
+ */
+export interface PostMembersResponse {
+    results_id: string;
+}
+
+/**
+ * TODO: investigate other discrepancies between this response and normal APIMembers
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#fetch-add-results
+ */
+export interface GetMembersAddResultsResponse {
+    members: (APIMember & { guid: string })[];
+}
+
+/**
+ * TODO: does this work with a DELETE request? that would make more sense here
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#remove-member
+ */
+export type PostMemberRemoveBody = never;
+
+/**
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#remove-member
+ */
+export type PostMemberRemoveResponse = never;
+
+/**
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#change-nickname
+ */
+export interface PostMemberUpdateBody {
+    membership: {
+        nickname: string;
+    }
+}
+
+/**
+ * TODO: investigate possible discrepancies
+ * https://github.com/2CATteam/GroupMeCommunityDocs/blob/master/members.md#change-nickname
+ */
+export type PostMemberUpdateResponse = APIMember;
