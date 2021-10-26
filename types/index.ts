f93ff9d61b1f132a6b5d8202cff40c80d909d68c
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-10-26 16:04:35 -0400

    add index.ts

---
+++ types/index.ts
@@ -0,0 +1,7 @@
+export * from "./APIAttachment";
+export * from "./APIGroup";
+export * from "./APIGroupEvent";
+export * from "./APIMember";
+export * from "./APIMessage";
+export * from "./APIPoll";
+export * from "./powerups";
