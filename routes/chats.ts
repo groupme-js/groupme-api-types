7d2fdbc805df7cccbfd4bc6ba7fd56dbb1e20005
Peter <43360094+not-so-smart@users.noreply.github.com>
2021-11-16 13:30:05 -0500

    [feat] add GetChatMessagesQuery interface

--- routes/chats.ts
+++ routes/chats.ts
@@ -8,6 +8,17 @@ export type GetChatsResponse = APIChat[];
 /**
  * https://github.com/groupme-js/GroupMeCommunityDocs/blob/master/dms.md#index-messages
  */
+export interface GetChatMessagesQuery {
+    other_user_id: string;
+    before_id?: string;
+    since_id?: string;
+    after_id?: string;
+    limit?: number;
+}
+
+/**
+ * https://github.com/groupme-js/GroupMeCommunityDocs/blob/master/dms.md#index-messages
+ */
 export interface GetChatMessagesResponse {
     count: number;
     direct_messages: APIChatMessage[];
